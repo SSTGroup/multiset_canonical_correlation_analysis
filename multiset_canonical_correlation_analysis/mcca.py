806544217c843a6df856734b0714b3e6ae8eb3c8
Isi Lehmann <isabell.lehmann@sst.upb.de>
2024-08-14 08:19:14 +0200

    simplify mcca algorithm code

--- multiset_canonical_correlation_analysis/mcca.py
+++ multiset_canonical_correlation_analysis/mcca.py
@@ -34,13 +34,13 @@ def mcca(X, algorithm='genvar'):
     if algorithm == 'sumcor':
         M, Epsilon = mcca_sumcorr_nielsen(X)
     elif algorithm == 'maxvar':
-        M, Epsilon = mcca_maxvar_kettenring(X)
+        M, Epsilon = mcca_maxvar_minvar_kettenring(X, 'maxvar')
     elif algorithm == 'minvar':
-        M, Epsilon = mcca_minvar_kettenring(X)
+        M, Epsilon = mcca_maxvar_minvar_kettenring(X, 'minvar')
     elif algorithm == 'ssqcor':
-        M, Epsilon = mcca_ssqcor_kettenring(X)
+        M, Epsilon = mcca_ssqcor_genvar_kettenring(X, 'ssqcor')
     elif algorithm == 'genvar':
-        M, Epsilon = mcca_genvar_kettenring(X)
+        M, Epsilon = mcca_ssqcor_genvar_kettenring(X, 'genvar')
     else:
         raise AssertionError("'algorithm' must be 'sumcor', 'maxvar', 'minvar', 'ssqcor', or 'genvar'.")
 
@@ -110,9 +110,9 @@ def mcca_sumcorr_nielsen(X):
     return M, Epsilon
 
 
-def mcca_maxvar_kettenring(X):
+def mcca_maxvar_minvar_kettenring(X, algorithm):
     """
-    Implementation of mCCA-maxvar according to
+    Implementation of mCCA-maxvar and mCCA-minvar according to
     Kettenring, J. R. (1971). Canonical analysis of several sets of variables. Biometrika, 58(3), 433-451.
 
     Info
@@ -123,6 +123,9 @@ def mcca_maxvar_kettenring(X):
     ----------
     X : np.ndarray
         Datasets of dimensions N x T x K, where X[:,:,k] corresponds to X^[k]
+
+    algorithm: str
+        'maxvar' or 'minvar'
 
     Returns
     -------
@@ -174,102 +177,22 @@ def mcca_maxvar_kettenring(X):
         eigval, eigvec = eigh(H_n @ C_yy @ H_n)
         # eigvals and eigvecs should be sorted in descending order
         eigvec = eigvec[:, ::-1]
-        # eigenvector corresponding to largest eigenvalue
-        V[:, n] = eigvec[:, 0]
 
-        # normalize each v_n^[k] -> w_n[k] has unit norm
-        for k in range(K):
-            v_n_k = V[k * N:(k + 1) * N, n]
-            V_tilde[:, n, k] = v_n_k / np.linalg.norm(v_n_k)
-
-    # now all b_n^[k] are calculated (Kettenring: j_b^(s), where k=j and n=s)
-
-    # calculating demixing matrices to multiply with x^[k] instead of y^[k]
-    M = np.zeros_like(V_tilde)
-    for k in range(K):
-        M[:, :, k] = np.linalg.inv(sqrtm(np.cov(X[:, :, k], ddof=0))) @ V_tilde[:, :, k]
-
-    # calculate canonical variates (they already have unit variance)
-    Epsilon = np.zeros_like(Y)
-    for k in range(K):
-        Epsilon[:, :, k] = M[:, :, k].T @ X[:, :, k]
-
-    return M, Epsilon
-
-
-def mcca_minvar_kettenring(X):
-    """
-    Implementation of mCCA-minvar according to
-    Kettenring, J. R. (1971). Canonical analysis of several sets of variables. Biometrika, 58(3), 433-451.
-
-    Info
-    ----
-    N: number of components, T: number of samples, K: number of datasets
-
-    Parameters
-    ----------
-    X : np.ndarray
-        Datasets of dimensions N x T x K, where X[:,:,k] corresponds to X^[k]
-
-    Returns
-    -------
-    M : np.ndarray
-        Transformation matrices of dimension N x N x K, such that E^[k] = (M^[k])^T X^[k]
-
-    Epsilon : np.ndarray
-        Canonical variables of dimension N x T x K
-
-    """
-
-    N, T, K = X.shape
-
-    # make sure data is zero-mean
-    check_zero_mean(X)
-
-    # whiten x^[k] -> y^[k] (using Mahalanobis whitening)
-    Y = np.zeros_like(X)
-    for k in range(K):
-        # ddof=0 means dividing by T, not T-1
-        Y[:, :, k] = np.linalg.inv(sqrtm(np.cov(X[:, :, k], ddof=0))) @ X[:, :, k]
-
-    # concatenate whitened datasets vertically
-    Y_concat = vectorize_datasets(Y)
-
-    # calculate C_yy = E[y y^T]
-    C_yy = np.cov(Y_concat, ddof=0)
-
-    # calculate the canonical variates in a deflationary way for each stage n = 1...N
-    V = np.zeros((N * K, N))
-    V_tilde = np.zeros((N, N, K))
-    for n in range(N):
-
-        if n == 0:
-            # H_n for the first stage is the identity matrix
-            H_n = np.eye(N * K)
+        if algorithm == 'maxvar':
+            # eigenvector corresponding to largest eigenvalue
+            V[:, n] = eigvec[:, 0]
+        elif algorithm == 'minvar':
+            # eigenvector corresponding to smallest non-zero eigenvalue
+            V[:, n] = eigvec[:, N * K - n * K - 1]
         else:
-            # update H_n matrix
-
-            # stack K partitions of V_tilde as diagonal blocks of V_tilde_n1 (of dimensions NK x (n-1)K )
-            # Kettenring eq. 9.9: D_c = diag{1_C^(s), ..., m_C^{(s)}, j_C^(s) = {j_b^(1), ..., j_b^(s-1)}
-            V_list = [V_tilde[:, 0:n, k] for k in range(K)]  # V_tilde is defined later
-            V_tilde_n1 = block_diag(*V_list)
-
-            # update H_n
-            H_n = np.eye(N * K) - V_tilde_n1 @ np.linalg.inv(V_tilde_n1.T @ V_tilde_n1) @ V_tilde_n1.T
-
-        # calc v_n as first eigvec of EVD(H_n R H_n), where H_1 = I
-        eigval, eigvec = eigh(H_n @ C_yy @ H_n)
-        # eigvals and eigvecs should be sorted in descending order
-        eigvec = eigvec[:, ::-1]
-        # eigenvector corresponding to smallest non-zero eigenvalue
-        V[:, n] = eigvec[:, N * K - n * K - 1]
+            raise ValueError("'algorithm' must be 'maxvar' or 'minvar'")
 
         # normalize each v_n^[k] -> w_n[k] has unit norm
         for k in range(K):
             v_n_k = V[k * N:(k + 1) * N, n]
             V_tilde[:, n, k] = v_n_k / np.linalg.norm(v_n_k)
 
-    # now all b_n^[k] are calculated (Kettenring: j_b^(s), where k=j and n=s)
+    # now all m_n^[k] are calculated (Kettenring: j_b^(s), where k=j and n=s)
 
     # calculating demixing matrices to multiply with x^[k] instead of y^[k]
     M = np.zeros_like(V_tilde)
@@ -284,103 +207,9 @@ def mcca_minvar_kettenring(X):
     return M, Epsilon
 
 
-def mcca_ssqcor_kettenring(X, max_iter=1000, eps=0.0001, verbose=False):
+def mcca_ssqcor_genvar_kettenring(X, algorithm, max_iter=1000, eps=0.0001, verbose=False):
     """
-    Implementation of mCCA-ssqcor according to
-    Kettenring, J. R. (1971). Canonical analysis of several sets of variables. Biometrika, 58(3), 433-451.
-
-    Info
-    ----
-    N: number of components, T: number of samples, K: number of datasets
-
-    Parameters
-    ----------
-    X : np.ndarray
-        Datasets of dimensions N x T x K, where X[:,:,k] corresponds to X^[k]
-
-    max_iter : int, optional
-        Maximum number of iterations before stopping the optimization
-
-    eps : float, optional
-        Threshold value for converge. If change of theta parameter is smaller than eps, optimization will stop.
-
-    verbose : bool, optional
-        If True, print after how many iterations the algorithm stopped for each SCV
-
-    Returns
-    -------
-    M : np.ndarray
-        Transformation matrices of dimension N x N x K, such that E^[k] = (M^[k])^T X^[k]
-
-    Epsilon : np.ndarray
-        Canonical variables of dimension N x T x K
-
-    """
-
-    N, T, K = X.shape
-
-    # make sure data is zero-mean
-    check_zero_mean(X)
-
-    # whiten x^[k] -> y^[k] (using Mahalanobis whitening)
-    Y = np.zeros_like(X)
-    for k in range(K):
-        # ddof=0 means dividing by T, not T-1
-        Y[:, :, k] = np.linalg.inv(sqrtm(np.cov(X[:, :, k], ddof=0))) @ X[:, :, k]
-
-    # concatenate whitened datasets vertically
-    Y_concat = vectorize_datasets(Y)
-
-    # calculate C_yy = E[y y^T]
-    C_yy = np.cov(Y_concat, ddof=0)
-
-    # initialize transformation matrices (that would transform Y)
-    V_tilde = 1 / np.sqrt(N) * np.ones((N, N, K))
-
-    for n in range(N):
-        theta_n = np.zeros((K, max_iter))
-        for iter in range(max_iter):
-            for k in range(K):
-                if n == 0:
-                    H_n_k = np.eye(N)  # A_j in eq. (12.5c) for the first CCV
-                else:
-                    V_n_1_k = V_tilde[:, 0:n, k]  # C_j on p.445
-                    H_n_k = np.eye(N) - V_n_1_k @ np.linalg.inv(V_n_1_k.T @ V_n_1_k) @ V_n_1_k.T  # A_j in eq. (12.5c)
-
-                # [ C_yy^[k,1] m_n^[1], ..., C_yy^[k,k-1] m_n^[k-1], C_yy^[k,k+1] m_n^[k+1], ..., C_yy^[k,K] m_n^[K] ]
-                N_n_k = []
-                for l in range(K):
-                    if l != k:
-                        N_n_k.append(C_yy[k * N: (k + 1) * N, l * N: (l + 1) * N] @ V_tilde[:, n, l])
-                N_n_k = np.array(N_n_k).T  # called N_j in Kettenring's paper p.445, his j is our k
-                P_n_k = N_n_k @ N_n_k.T  # P_j on p.445
-
-                # we can perform EVD of H_n F_n H_n, as we are just interested in the leading eigenvector
-                lambda_n_k, V_n_k = np.linalg.eigh(H_n_k @ P_n_k @ H_n_k)
-                V_tilde[:, n, k] = V_n_k[:, -1]
-                theta_n[k, iter] = 1 + lambda_n_k[-1]
-
-            if np.sum(np.abs(theta_n[:, iter] - theta_n[:, iter - 1])) < eps or iter == max_iter:  # eq. (12.9)
-                if verbose:
-                    print(f'Stopping for the {n}th CCV after {iter} iterations')
-                break
-
-    # find transformation matrices for X^[k] instead of Y^[k]
-    M = np.zeros((N, N, K))
-    for k in range(K):
-        M[:, :, k] = np.linalg.inv(sqrtm(np.cov(X[:, :, k], ddof=0))) @ V_tilde[:, :, k]
-
-    # calculate canonical variates (they already have unit variance)
-    Epsilon = np.zeros_like(X)
-    for k in range(K):
-        Epsilon[:, :, k] = M[:, :, k].T @ X[:, :, k]
-
-    return M, Epsilon
-
-
-def mcca_genvar_kettenring(X, max_iter=1000, eps=0.0001, verbose=False):
-    """
-    Implementation of mCCA-genvar according to
+    Implementation of mCCA-ssqcor and mCCA-genvar according to
     Kettenring, J. R. (1971). Canonical analysis of several sets of variables. Biometrika, 58(3), 433-451.
 
     Info
@@ -437,9 +266,10 @@ def mcca_genvar_kettenring(X, max_iter=1000, eps=0.0001, verbose=False):
             for k in range(K):
                 if n == 0:
                     H_n_k = np.eye(N)  # A_j in eq. (12.5c) for the first CCV
-                    # init R_n
-                    B_n = block_diag(*V_tilde[:, n, :].T).T
-                    R_n = B_n.T @ C_yy @ B_n  # K x K covariance matrix of nth CCV
+                    if algorithm == 'genvar':
+                        # init R_n
+                        B_n = block_diag(*V_tilde[:, n, :].T).T
+                        R_n = B_n.T @ C_yy @ B_n  # K x K covariance matrix of nth CCV
                 else:
                     V_n_1_k = V_tilde[:, 0:n, k]  # C_j on p.445
                     H_n_k = np.eye(N) - V_n_1_k @ np.linalg.inv(V_n_1_k.T @ V_n_1_k) @ V_n_1_k.T  # A_j in eq. (12.5c)
@@ -451,18 +281,24 @@ def mcca_genvar_kettenring(X, max_iter=1000, eps=0.0001, verbose=False):
                         N_n_k.append(C_yy[k * N: (k + 1) * N, l * N: (l + 1) * N] @ V_tilde[:, n, l])
                 N_n_k = np.array(N_n_k).T  # called N_j in Kettenring's paper p.445, his j is our k
 
-                # delete kth row and kth column of nth CCV covariance matrix R_n
-                R_n_minus_k = np.delete(np.delete(R_n, k, 0), k, 1)  # K-1 x K-1 matrix Phi_j on p.445, his j = our k
-                F_n_k = N_n_k @ np.linalg.inv(R_n_minus_k) @ N_n_k.T  # Q_j on p.445
+                if algorithm == 'ssqcor':
+                    F_n_k = N_n_k @ N_n_k.T  # P_j on p.445
+                elif algorithm == 'genvar':
+                    # delete kth row and kth column of nth CCV covariance matrix R_n
+                    R_n_minus_k = np.delete(np.delete(R_n, k, 0), k, 1)  # K-1 x K-1 matrix Phi_j on p.445, his j=our k
+                    F_n_k = N_n_k @ np.linalg.inv(R_n_minus_k) @ N_n_k.T  # Q_j on p.445
+                else:
+                    raise ValueError("'algorithm' must be 'ssqcor' or 'genvar'")
 
                 # we can perform EVD of H_n F_n H_n, as we are just interested in the leading eigenvector
                 lambda_n_k, V_n_k = np.linalg.eigh(H_n_k @ F_n_k @ H_n_k)
-                V_tilde[:, n, k] = V_n_k[:, -1]
+                V_tilde[:, n, k] = V_n_k[:, -1]  # -1 as the eigenvalues are ascending -> last is largest
                 theta_n[k, iter] = 1 + lambda_n_k[-1]
 
-                # update R_n
-                B_n = block_diag(*V_tilde[:, n, :].T).T
-                R_n = B_n.T @ C_yy @ B_n  # K x K covariance matrix of nth CCV
+                if algorithm == 'genvar':
+                    # update R_n
+                    B_n = block_diag(*V_tilde[:, n, :].T).T
+                    R_n = B_n.T @ C_yy @ B_n  # K x K covariance matrix of nth CCV
 
             if np.sum(np.abs(theta_n[:, iter] - theta_n[:, iter - 1])) < eps or iter == max_iter:  # eq. (12.9)
                 if verbose:
